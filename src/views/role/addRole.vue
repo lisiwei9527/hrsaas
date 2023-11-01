075d66adf1a8e3976fdde42f52245b2f5973e221
lisiwei <1147766324@qq.com>
2023-11-01 16:13:43 +0800

    新增角色页面

---
+++ src/views/role/addRole.vue
@@ -0,0 +1,19 @@
+<script setup>
+function test(a) {
+  console.log(a)
+  var a = 1
+  console.log(a)
+  function a() {}
+  console.log(a)
+  var b = function () {}
+  console.log(b)
+  function d() {}
+}
+
+test(2)
+</script>
+<template>
+  <div>打开控制台</div>
+</template>
+
+<style lang="scss" scoped></style>
