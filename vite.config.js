6a06bbfac8c63754c543cca8f9f781a48076a99c
lisiwei <1147766324@qq.com>
2023-11-02 15:22:18 +0800

    doc: add tailwind config file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss, autoprefixer],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -19,6 +21,9 @@ export default defineConfig({
         },
         javascriptEnabled: true
       }
+    },
+    postcss: {
+      plugins: [tailwindcss, autoprefixer]
     }
   },
   server: {
